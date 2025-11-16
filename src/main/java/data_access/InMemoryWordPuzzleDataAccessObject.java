fd5b7c25aec5b9d9500af17f3aaa63efab9d0108
Joe <joex.lin@mail.utoronto.ca>
2025-11-16 01:12:49 -0500

    added new comments

--- src/main/java/data_access/InMemoryWordPuzzleDataAccessObject.java
+++ src/main/java/data_access/InMemoryWordPuzzleDataAccessObject.java
@@ -1,4 +1,38 @@
 package data_access;
 
-public class InMemoryWordPuzzleDataAccessObject {
+import entity.WordPuzzle;
+import entity.Guess;
+import use_case.MakeGuess.MakeGuessWordPuzzleDataAccessInterface;
+
+public class InMemoryWordPuzzleDataAccessObject implements MakeGuessWordPuzzleDataAccessInterface{
+
+    private WordPuzzle currentPuzzle;
+
+    public InMemoryWordPuzzleDataAccessObject(WordPuzzle initialPuzzle) {
+        this.currentPuzzle = initialPuzzle;
+    }
+
+    @Override
+    public WordPuzzle getCurrentWordPuzzle() {
+        return currentPuzzle;
+    }
+
+    @Override
+    public boolean isGuessCorrect(Guess guess){
+        // If the letter in the guess corresponds to a hidden letter in the puzzle, this is a correct guess
+        return this.currentPuzzle.isLetterHidden(guess.getLetter());
+    }
+
+    @Override
+    // Reveal the given letter in the word puzzle
+    public void revealLetter(Guess guess) {
+        this.currentPuzzle.revealLetter(guess.getLetter());
+    }
+
+    @Override
+    // Check if the puzzle is completed
+    public boolean isPuzzleComplete() {
+        return this.currentPuzzle.isPuzzleComplete();
+    }
+
 }
--- src/main/java/use_case/MakeGuess/MakeGuessInteractor.java
+++ src/main/java/use_case/MakeGuess/MakeGuessInteractor.java
@@ -1,4 +1,37 @@
 package use_case.MakeGuess;
 
-public class MakeGuessInteractor {
+import entity.Guess;
+
+public class MakeGuessInteractor implements MakeGuessInputBoundary {
+
+    private final MakeGuessOutputBoundary presenter;
+    private final MakeGuessWordPuzzleDataAccessInterface wordPuzzleDAO;
+
+    public MakeGuessInteractor(MakeGuessOutputBoundary presenter,  MakeGuessWordPuzzleDataAccessInterface wordPuzzleDAO) {
+        this.presenter = presenter;
+        this.wordPuzzleDAO = wordPuzzleDAO;
+    }
+
+    @Override
+    public void execute(MakeGuessInputData inputData) {
+
+        Guess guess = inputData.getGuess();
+
+        // Check if the letter in the guess exist in the word puzzle
+        boolean isGuessCorrect = wordPuzzleDAO.isGuessCorrect(guess);
+
+        boolean isPuzzleComplete = false;
+
+        // If the guess is correct, reveal the correctly guessed letter and check if the puzzle is complete
+        if (isGuessCorrect) {
+            this.wordPuzzleDAO.revealLetter(guess);
+            isPuzzleComplete = this.wordPuzzleDAO.isPuzzleComplete();
+        }
+
+        MakeGuessOutputData outputData =
+                new MakeGuessOutputData(guess, isGuessCorrect, isPuzzleComplete);
+
+        //TODO: implement the presenter
+        presenter.updateView(outputData);
+    }
 }
