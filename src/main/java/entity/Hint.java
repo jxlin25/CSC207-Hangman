44f77a5293063782c9776494442c664dd28ba03c
Zirui Xu <xuzirui866@gmail.com>
2025-11-15 22:39:44 -0500

    The files have been rearranged

---
+++ src/main/java/entity/Hint.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Hint {
+    private final String hint;
+
+    public Hint(String hint) {
+        this.hint = hint;
+    }
+
+    public String gethint() {
+        return hint;
+    }
+}
