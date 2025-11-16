fb005f5ecfcb6b547f43dc1a5bf4c1336909c3f8
Zirui Xu <xuzirui866@gmail.com>
2025-11-16 03:10:25 -0500

    Is Demo of hint, but not finish

--- src/main/java/data_access/GeminiHintDataAccessObject.java
+++ src/main/java/data_access/GeminiHintDataAccessObject.java
@@ -7,15 +7,11 @@ public class GeminiHintDataAccessObject implements HintDataAccessInterface {
     private static final String CONTENT_TYPE_JSON = "application/json";
     private static final String CONTENT_TYPE_LABEL = "Content-Type";
     private static final int SUCCESS_CODE = 200;
-    private final String apiKey;
 
-    public GeminiHintDataAccessObject(String apiKey) {
-        this.apiKey = apiKey;
-    }
+    public GeminiHintDataAccessObject() {}
 
     @Override
     public String getHint(String word) {
         return "nothing";
     }
-
 }
